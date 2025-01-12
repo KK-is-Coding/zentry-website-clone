32dbe8b6e7b7c98067d0f058929da7b3fa9baa5d
Krishna Kesarwani <99733616+KK-is-Coding@users.noreply.github.com>
2025-01-12 21:14:29 +0530

    changed the loader design

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -93,10 +93,21 @@ const Hero = () => {
             {loading && (
                 <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
                     {/* https://uiverse.io/G4b413l/tidy-walrus-92 */}
-                    <div className="three-body">
-                        <div className="three-body__dot"></div>
-                        <div className="three-body__dot"></div>
-                        <div className="three-body__dot"></div>
+                    <div className="particle-box">
+
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
+                        <div class="particle"></div>
                     </div>
                 </div>
             )}
